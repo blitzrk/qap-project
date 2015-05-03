5bd3fb4c82d5f294fe945d0644f0a317fe47ff28
Ben Krieger <blitzrk@gmail.com>
2015-05-03 15:43:53 -0600

    format and keep data in the package that uses it

--- generate.go
+++ generate.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "generate/dat"
+	"fmt"
+	"github.com/blitzrk/generate/dat"
+	"io/ioutil"
 )
 
 func main() {
-  f, err := ioutil.ReadFile("bur26a.dat")
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  
-  data := dat.Read(f)
-  n := data[0][0][0]
-  times := data[1]
-  freqs := data[2]
-  
-  fmt.Println(n)
-  fmt.Println()
-  fmt.Println(times)
-  fmt.Println()
-  fmt.Println(freqs)
+	f, err := ioutil.ReadFile("bur26a.dat")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	data := dat.Read(f)
+	n := data[0][0][0]
+	times := data[1]
+	freqs := data[2]
+
+	fmt.Println(n)
+	fmt.Println()
+	fmt.Println(times)
+	fmt.Println()
+	fmt.Println(freqs)
 }
