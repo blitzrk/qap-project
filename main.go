b47fb2a988b3016522185157fec53cdb5e2671ec
Ben Krieger <blitzrk@gmail.com>
2015-05-07 15:36:47 -0600

    added data logging to main

--- main.go
+++ main.go
@@ -4,21 +4,30 @@ import (
 	"fmt"
 	"github.com/blitzrk/qap-project/data"
 	"github.com/blitzrk/qap-project/search"
+	"log"
+	"os"
 	"runtime"
 	"time"
 )
 
 var (
-	fact func(int) uint
+	fact   func(int) uint
+	logger *log.Logger
 )
 
 func init() {
 	fact = factorial()
+
+	file, err := os.OpenFile("data.log", os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open log file", os.Stderr, ":", err)
+	}
+	logger = log.New(file, "", log.Lshortfile)
 }
 
 func main() {
 	// Setup data generator
-	n := 7
+	n := 8
 	gen := data.New(n, 100000)
 
 	// Generate data
@@ -34,6 +43,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	logger.Println("Distance matrix:")
+	logger.Println(dist)
+	logger.Println()
+	logger.Println("Flow matrix:")
+	logger.Println(flow)
 
 	// Setup runner
 	maxTime := time.NewTimer(15 * time.Minute)
--- matrix/matrix.go
+++ matrix/matrix.go
@@ -2,6 +2,8 @@ package matrix
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 )
 
 var (
@@ -14,6 +16,16 @@ type Element float64
 type Matrix [][]Element
 
 type Matrix4D [][][][]Element
+
+func (m Matrix) String() string {
+	var s []string
+
+	for _, row := range m {
+		s = append(s, fmt.Sprint(row))
+	}
+
+	return strings.Join(s, "\n")
+}
 
 func (m Matrix) At(r, c int) Element {
 	return m[r][c]
